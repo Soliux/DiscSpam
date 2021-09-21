d30d58592076a29e1c9e0fcc8bd35d5670ce1a00
M1000a <66280234+M1000a@users.noreply.github.com>
2021-09-21 19:18:59 +0100

    .

---
+++ interact/reactions.go
@@ -0,0 +1,98 @@
+package interact
+
+import (
+	"Raid-Client/cloudflare"
+	"Raid-Client/utils"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+)
+
+var cmt int
+
+func AddReaction(ChannelID string, MessageID string, Token string, Emoji string) error {
+	if cmt >= 2 {
+		return errors.New("error working")
+	} else {
+		request, err := http.NewRequest("PUT", fmt.Sprintf("https://discord.com/api/v9/channels/%s/messages/%s/reactions/%s/%s", ChannelID, MessageID, Emoji, "%40me"), nil)
+		if err != nil {
+			return err
+		}
+		cf := cloudflare.Cookie()
+		xprop := utils.XSuperProperties()
+		request.Header = http.Header{
+			"Accept":             []string{"*/*"},
+			"Accept-language":    []string{"en-GB"},
+			"Authorization":      []string{Token},
+			"Content-length":     []string{"2"},
+			"Content-type":       []string{"application/json"},
+			"Cookie":             []string{cf},
+			"Origin":             []string{"https://discord.com"},
+			"Sec-fetch-dest":     []string{"empty"},
+			"Sec-fetch-mode":     []string{"cors"},
+			"Sec-fetch-site":     []string{"same-origin"},
+			"User-agent":         []string{"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.15 Chrome/83.0.4103.122 Electron/9.3.5 Safari/537.36"},
+			"X-debug-options":    []string{"bugReporterEnabled"},
+			"X-super-properties": []string{xprop},
+		}
+
+		client := &http.Client{
+			Timeout: 5 * time.Second,
+		}
+
+		res, err := client.Do(request)
+		if err != nil {
+			return err
+		}
+
+		switch res.StatusCode {
+		case 204:
+			fmt.Printf("%s %s %s\n", red(Token), green("Success:"), white(fmt.Sprintf("Added reaction %s to %s", Emoji, MessageID)))
+			cmt--
+		default:
+			fmt.Printf("%s %s\n", white(Token), red(fmt.Sprintf("Error: Unable to add reaction %s to %s", Emoji, MessageID)))
+			cmt++
+		}
+
+		return nil
+	}
+}
+
+func ReactionMessage(ChannelID string, MessageID string, Token string, Word string) error {
+	letters := map[string]string{
+		"A": "🇦",
+		"B": "🇧",
+		"C": "🇨",
+		"D": "🇩",
+		"E": "🇪",
+		"F": "🇫",
+		"G": "🇬",
+		"H": "🇭",
+		"I": "🇮",
+		"J": "🇯",
+		"K": "🇰",
+		"L": "🇱",
+		"M": "🇲",
+		"N": "🇳",
+		"O": "🇴",
+		"P": "🇵",
+		"Q": "🇶",
+		"R": "🇷",
+		"S": "🇸",
+		"T": "🇹",
+		"U": "🇺",
+		"V": "🇻",
+		"W": "🇼",
+		"X": "🇽",
+		"Y": "🇾",
+		"Z": "🇿",
+	}
+	for _, letter := range Word {
+		l := strings.ToUpper(string(letter))
+		AddReaction(ChannelID, MessageID, Token, letters[l])
+		time.Sleep(1 * time.Second)
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -21,11 +21,18 @@ func main() {
 	// if err != nil {
 	// 	log.Fatal(err)
 	// }
-	err := interact.SendMessage("889537520688332851", "889537520688332855", "ODg5NTUzMTY2MTU5NDUwMTY0.YUi7nA.6yXPQf3fWb-qFj6pncNP97Ie_pk", "Hello from other side")
+	// err := interact.SendMessage("889537520688332851", "889537520688332855", "ODg5NTUzMTY2MTU5NDUwMTY0.YUi7nA.6yXPQf3fWb-qFj6pncNP97Ie_pk", "Hello from other side")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// err := interact.AddReaction("889537520688332855", "889913197119828019", "ODg5NTUzMTY2MTU5NDUwMTY0.YUi7nA.6yXPQf3fWb-qFj6pncNP97Ie_pk", "🇦")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	err := interact.ReactionMessage("889537520688332855", "889913197119828019", "ODg5NTUzMTY2MTU5NDUwMTY0.YUi7nA.6yXPQf3fWb-qFj6pncNP97Ie_pk", "gay")
 	if err != nil {
 		log.Fatal(err)
 	}
-
 }
 
 func init() {
