3626b3e34927bb3e7253de0f24c2bf50ff0d2eda
bytixo <69140663+bytixo@users.noreply.github.com>
2021-10-19 20:00:55 +0200

    Create checkTokens.go

---
+++ utils/checkTokens.go
@@ -0,0 +1,70 @@
+package utils
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/gookit/color"
+)
+
+var (
+	wg         sync.WaitGroup
+	mutex      sync.Mutex
+	goodTokens []string
+	good       int
+	bad        int
+	locked     int
+
+	green = color.FgGreen.Render
+	white = color.FgWhite.Render
+	red   = color.FgRed.Render
+)
+
+func CheckTokens(tokens []string) []string {
+	fmt.Println("Checking:", len(tokens))
+	for _, t := range tokens {
+		wg.Add(1)
+		go func(t string) {
+			mutex.Lock()
+			defer mutex.Unlock()
+			defer wg.Done()
+			request, err := http.NewRequest("GET", "https://discord.com/api/v9/users/@me/guild-events", nil)
+			if err != nil {
+				fmt.Println(err)
+			}
+			request.Header = http.Header{
+				"Authorization": []string{t},
+				"Content-type":  []string{"application/json"},
+			}
+			client := &http.Client{
+				Timeout: 5 * time.Second,
+			}
+			res, err := client.Do(request)
+			if err != nil {
+				fmt.Println(err)
+			}
+			defer res.Body.Close()
+
+			switch res.StatusCode {
+			case 200:
+				good++
+				fmt.Printf("%s %s \n", white(t), green("| is Valid"))
+				goodTokens = append(goodTokens, t)
+			case 401:
+				bad++
+				fmt.Printf("%s %s \n", white(t), red("| is Invalid"))
+			case 403:
+				locked++
+				fmt.Printf("%s %s \n", white(t), red("| is Phone locked"))
+			default:
+				bad++
+				fmt.Printf("%s %s \n", white(t), red("| is Invalid"))
+			}
+		}(t)
+	}
+	wg.Wait()
+	fmt.Println("Finished Checking\nGood: tokens", good, "\nBad tokens:", bad, "\nLocked tokens:", locked)
+	return goodTokens
+}
